c242f7e46a33513e71f61f21e3799087370e27b9
we333 <wantone321@gmail.com>
2017-06-02 17:12:04 +0900

    10k training completed, test not very well

---
+++ test.py
@@ -0,0 +1,85 @@
+import os
+import numpy as np
+import tensorflow as tf
+import load_data
+import model
+
+import cv2
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def get_one_image(train):
+    '''Randomly pick one image from training data
+    Return: ndarray
+    '''
+    n = len(train)
+
+    print n
+
+    ind = np.random.randint(0, n)
+    img_dir = train[ind]
+
+    print img_dir
+
+    image = Image.open(img_dir)
+    print '---------------------------'
+    plt.imshow(image)
+
+    image = image.resize([208, 208])
+    image = np.array(image)
+    return image
+
+def evaluate_one_image():
+    '''Test one image against the saved models and parameters
+    '''
+    
+    # you need to change the directories to yours.
+    train_dir = './genki4k/'
+    train, train_label = load_data.get_files(train_dir)
+    image_array = get_one_image(train)
+    
+    with tf.Graph().as_default():
+        BATCH_SIZE = 1
+        N_CLASSES = 2
+        
+        image = tf.cast(image_array, tf.float32)
+        image = tf.image.per_image_standardization(image)
+        image = tf.reshape(image, [1, 208, 208, 3])
+        logit = model.inference(image, BATCH_SIZE, N_CLASSES)
+        
+        logit = tf.nn.softmax(logit)
+        
+        x = tf.placeholder(tf.float32, shape=[208, 208, 3])
+        
+        # you need to change the directories to yours.
+        logs_train_dir = './logs/train/' 
+                       
+        saver = tf.train.Saver()
+        
+        with tf.Session() as sess:
+            
+            print("Reading checkpoints...")
+            ckpt = tf.train.get_checkpoint_state(logs_train_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                print('Loading success, global_step is %s' % global_step)
+            else:
+                print('No checkpoint file found')
+            
+            prediction = sess.run(logit, feed_dict={x: image_array})
+
+            print prediction
+
+            max_index = np.argmax(prediction)
+            if max_index==0:
+                print('This is smile with possibility %.6f' %prediction[:, 0])
+            else:
+                print('This is non-smile with possibility %.6f' %prediction[:, 1])
+    return image_array
+
+
+image_array = evaluate_one_image()
+
+print '&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
--- training.py
+++ training.py
@@ -48,7 +48,7 @@ def run_training():
                     break
             _, tra_loss, tra_acc = sess.run([train_op, train_loss, train__acc])
                
-            if step % 50 == 0:
+            if step % 10 == 0:
                 print('Step %d, train loss = %.2f, train accuracy = %.2f%%' %(step, tra_loss, tra_acc*100.0))
                 summary_str = sess.run(summary_op)
                 train_writer.add_summary(summary_str, step)
@@ -64,65 +64,6 @@ def run_training():
         
     coord.join(threads)
     sess.close()
-
-
-from PIL import Image
-import matplotlib.pyplot as plt
-
-def get_one_image(train):
-    '''Randomly pick one image from training data
-    Return: ndarray
-    '''
-    n = len(train)
-    ind = np.random.randint(0, n)
-    img_dir = train[ind]
-
-    image = Image.open(img_dir)
-    plt.imshow(image)
-    image = image.resize([208, 208])
-    image = np.array(image)
-    return image
-
-def evaluate_one_image():
-    train_dir = './genki4k/'
-    train, train_label = load_data.get_files(train_dir)
-    image_array = get_one_image(train)
-    
-    with tf.Graph().as_default():
-        BATCH_SIZE = 1
-        N_CLASSES = 2
-        
-        image = tf.cast(image_array, tf.float32)
-        image = tf.image.per_image_standardization(image)
-        image = tf.reshape(image, [1, 208, 208, 3])
-        logit = model.inference(image, BATCH_SIZE, N_CLASSES)
-        
-        logit = tf.nn.softmax(logit)
-        
-        x = tf.placeholder(tf.float32, shape=[208, 208, 3])
-        
-        # you need to change the directories to yours.
-        logs_train_dir = './logs/train/' 
-                       
-        saver = tf.train.Saver()
-        
-        with tf.Session() as sess:
-            
-            print("Reading checkpoints...")
-            ckpt = tf.train.get_checkpoint_state(logs_train_dir)
-            if ckpt and ckpt.model_checkpoint_path:
-                global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
-                saver.restore(sess, ckpt.model_checkpoint_path)
-                print('Loading success, global_step is %s' % global_step)
-            else:
-                print('No checkpoint file found')
-            
-            prediction = sess.run(logit, feed_dict={x: image_array})
-            max_index = np.argmax(prediction)
-            if max_index==0:
-                print('This is smile with possibility %.6f' %prediction[:, 0])
-            else:
-                print('This is a nonsmile with possibility %.6f' %prediction[:, 1])
 
 
 run_training()
